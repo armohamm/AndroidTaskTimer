0755d4a0e7e68a0eb2c2e09024fa6df14385c3bb
tmosest <tmosest@gmail.com>
2018-12-28 14:51:04 -0500

    default project

--- app/src/main/java/com/tmosest/androidtasktimer/MainActivityFragment.kt
+++ app/src/main/java/com/tmosest/androidtasktimer/MainActivityFragment.kt
@@ -11,8 +11,10 @@ import android.view.ViewGroup
  */
 class MainActivityFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 }
